17775ecce1d79e5a9e9b30ce99febb2e432c5da0
Ivan Pidov <ivan.pidov@tick42.com>
2017-10-04 18:41:55 +0300

    Measurements form and reducer

--- client/containers/MeasurementsForm.vue
+++ client/containers/MeasurementsForm.vue
@@ -1,8 +1,60 @@
 <template>
   <el-form ref="form" label-width="120px">
-    <el-form-item label="Activity name">
-      <el-input v-model="weight"></el-input>
+    <el-form-item label="Height" >
+      <el-input :value="fields.height.value"@change="value => onInputChange('height', value)"></el-input>
     </el-form-item>
+    <el-form-item label="Weight">
+      <el-input :value="fields.weight.value" @change="value => onInputChange('weight', value)"></el-input>
+    </el-form-item>
+    <el-form-item label="Shoulders">
+      <el-input :value="fields.shoulders.value" @change="value => onInputChange('shoulders', value)"></el-input>
+    </el-form-item>
+    <el-form-item label="Chest">
+      <el-input :value="fields.chest.value" @change="value => onInputChange('chest', value)"></el-input>
+    </el-form-item>
+    <el-form-item label="Waist">
+      <el-input :value="fields.waist.value" @change="value => onInputChange('waist', value)"></el-input>
+    </el-form-item>
+    <el-col :span="12">
+      <el-form-item label="Hip">
+        <el-input :value="fields.hip.value" @change="value => onInputChange('hip', value)"></el-input>
+      </el-form-item>
+      <el-form-item label="Left Thigh">
+        <el-input :value="fields.leftThigh.value" @change="value => onInputChange('leftThigh', value)"></el-input>
+      </el-form-item>
+      <el-form-item label="Right Thigh">
+        <el-input :value="fields.rightThigh.value" @change="value => onInputChange('rightThigh', value)"></el-input>
+      </el-form-item>
+      <el-form-item label="Left Calf">
+        <el-input :value="fields.leftCalf.value" @change="value => onInputChange('leftCalf', value)"></el-input>
+      </el-form-item>
+      <el-form-item label="Right Calf">
+        <el-input :value="fields.rightCalf.value" @change="value => onInputChange('rightCalf', value)"></el-input>
+      </el-form-item>
+      <el-form-item label="Ankle">
+        <el-input :value="fields.ankle.value" @change="value => onInputChange('ankle', value)"></el-input>
+      </el-form-item>
+    </el-col>
+    <el-col :span="12">
+      <el-form-item label="Left Biceps">
+        <el-input :value="fields.leftBiceps.value" @change="value => onInputChange('leftBiceps', value)"></el-input>
+      </el-form-item>
+      <el-form-item label="Right Biceps">
+        <el-input :value="fields.rightBiceps.value" @change="value => onInputChange('rightBiceps', value)"></el-input>
+      </el-form-item>
+      <el-form-item label="Left Triceps">
+        <el-input :value="fields.leftTriceps.value" @change="value => onInputChange('leftTriceps', value)"></el-input>
+      </el-form-item>
+      <el-form-item label="Right Triceps">
+        <el-input :value="fields.rightTriceps.value" @change="value => onInputChange('rightTriceps', value)"></el-input>
+      </el-form-item>
+      <el-form-item label="Left Forearm">
+        <el-input :value="fields.leftForearm.value" @change="value => onInputChange('leftForearm', value)"></el-input>
+      </el-form-item>
+      <el-form-item label="Right Forearm">
+        <el-input :value="fields.rightForearm.value" @change="value => onInputChange('rightForearm', value)"></el-input>
+      </el-form-item>
+    </el-col>
     <!-- <el-form-item label="Activity time">
       <el-date-picker type="date" placeholder="Pick a date" v-model="form.date1" style="width: 100%;"></el-date-picker>
     </el-form-item> -->
@@ -10,16 +62,24 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
 
 export default {
   name: 'Measurements',
+  methods: {
+    ...mapActions(['setField']),
+    onInputChange(name, value) {
+      return this.setField({
+        formName: 'measurements',
+        fieldName: name,
+        fieldValue: value
+      })
+    }
+  },
   computed: {
-    ...mapGetters(['getField']),
-    ...{
-      weight() {
-        return this.getField('weight')
-      }
+    ...mapGetters(['inputFields']),
+    fields() {
+      return this.inputFields('measurements')
     }
   }
 }
--- client/store/form.js
+++ client/store/form.js
@@ -2,29 +2,39 @@ export const SET_FIELD = 'SET_FIELD'
 
 // state.forms.[formName].fields.[fieldName].value
 
-const state = {
-  fields: {
-    weight: {
-      value: 107
+const fields = ['height', 'weight', 'shoulders', 'chest', 'waist', 'leftBiceps', 'rightBiceps', 'leftTriceps', 'rightTriceps', 'leftForearm', 'rightForearm', 'hip', 'leftThigh', 'rightThigh', 'leftCalf', 'rightCalf', 'ankle']
+
+const mapFieldsToState = (fields) => fields.reduce((acc, field) => {
+  acc[field] = {
+    value: '',
+    error: {
+      message: '2',
+      hasError: false
     }
-  },
-  height: 192
+  }
+  return acc
+}, {})
+
+const initialState = {
+  measurements: {
+    fields: mapFieldsToState(fields)
+  }
 }
 
 const mutations = {
-  [SET_FIELD] (state) {
-
+  [SET_FIELD] (state, { formName, fieldName, fieldValue }) {
+    state[formName].fields[fieldName].value = fieldValue
   }
 }
 
 const actions = {
-  setField ({ commit }) {
-    commit({ type: SET_FIELD })
+  setField ({ commit }, payload) {
+    commit({ type: SET_FIELD, ...payload })
   }
 }
 
 const getters = {
-  getField: state => fieldName => state.fields[fieldName].value
+  inputFields: state => formName => state[formName].fields
 }
 
-export default { state, actions, mutations, getters }
+export default { state: initialState, actions, mutations, getters }
